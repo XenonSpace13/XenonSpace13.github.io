bcb5df3a89ee4b4ca5a1c643016c40e68c1433ae
XenonSpace13 <143076248+XenonSpace13@users.noreply.github.com>
2023-09-02 04:41:30 +0800

    favicon done + about us section in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+function getAttributeImgWidth() {
+	var width = main.getAttribute("width");
+	return 1
+}
 
 (function($) {
 
